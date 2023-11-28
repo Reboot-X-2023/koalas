8b9c9360f953f698d522352b335c9efdf35c6667
Angie <michailidou.ang@hotmail.com>
2023-11-28 14:14:53 +0000

    Add all new changes to main

--- src/app/lib/view_model/chat_screen_view_model.dart
+++ src/app/lib/view_model/chat_screen_view_model.dart
@@ -1,5 +1,6 @@
 // view_models/chat_screen_view_model.dart
 import 'dart:async';
+import 'package:http/http.dart' as http;
 
 class ChatScreenViewModel {
   final List<String> _userMessages = [];
@@ -16,7 +17,30 @@ class ChatScreenViewModel {
 
   void sendMessage(String message) {
     _userMessages.add(message);
+    _sendMessageToServer(message);
     _messageController.add(List.from(_userMessages));
+
+    // Send the message to the server
+    _sendMessageToServer(message);
+  }
+
+  Future<void> _sendMessageToServer(String message) async {
+    const String getUrl = 'http://localhost:8000/llm/'; // Replace with your actual API endpoint for GET request
+
+    try {
+      // Send a GET request
+      final getResponse = await http.get(Uri.parse(getUrl));
+
+      if (getResponse.statusCode == 200) {
+        print('GET request successful. Response: ${getResponse.body}');
+      } else {
+        print('Failed to make GET request. Status code: ${getResponse.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
   }
 }
+
+
 
